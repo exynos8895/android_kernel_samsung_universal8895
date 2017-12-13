47347aeaf536d1d7e532e6c9424e1edefd7b11ac
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-12-13 11:59:39 +0100

    Revert "USB: core: only clean up what we allocated"
    commit cf4df407e0d7cde60a45369c2a3414d18e2d4fdd upstream.
    
    This reverts commit 32fd87b3bbf5f7a045546401dfe2894dbbf4d8c3.
    
    Alan wrote a better fix for this...
    
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: stable <stable@vger.kernel.org>
    Cc: Nathan Chancellor <natechancellor@gmail.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/config.c
+++ drivers/usb/core/config.c
@@ -734,16 +734,14 @@ void usb_destroy_configuration(struct usb_device *dev)
 		return;
 
 	if (dev->rawdescriptors) {
-		for (i = 0; i < dev->descriptor.bNumConfigurations &&
-				i < USB_MAXCONFIG; i++)
+		for (i = 0; i < dev->descriptor.bNumConfigurations; i++)
 			kfree(dev->rawdescriptors[i]);
 
 		kfree(dev->rawdescriptors);
 		dev->rawdescriptors = NULL;
 	}
 
-	for (c = 0; c < dev->descriptor.bNumConfigurations &&
-			c < USB_MAXCONFIG; c++) {
+	for (c = 0; c < dev->descriptor.bNumConfigurations; c++) {
 		struct usb_host_config *cf = &dev->config[c];
 
 		kfree(cf->string);
