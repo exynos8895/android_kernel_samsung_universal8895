a2387153342e2347efde0f1b4370c3f537119c17
Geert Uytterhoeven <geert+renesas@glider.be>
2018-04-10 15:21:43 +0200

    ARM: amba: Make driver_override output consistent with other buses
    commit 5f53624662eaac89598641cee6cd54fc192572d9 upstream.
    
    For AMBA devices with unconfigured driver override, the
    "driver_override" sysfs virtual file is empty, while it contains
    "(null)" for platform and PCI devices.
    
    Make AMBA consistent with other buses by dropping the test for a NULL
    pointer.
    
    Note that contrary to popular belief, sprintf() handles NULL pointers
    fine; they are printed as "(null)".
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Cc: stable <stable@vger.kernel.org>
    Reviewed-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/amba/bus.c
+++ drivers/amba/bus.c
@@ -70,10 +70,7 @@ static ssize_t driver_override_show(struct device *_dev,
 	struct amba_device *dev = to_amba_device(_dev);
 	ssize_t len;
 
-	device_lock(_dev);
-	len = sprintf(buf, "%s\n", dev->driver_override);
-	device_unlock(_dev);
-	return len;
+	return sprintf(buf, "%s\n", dev->driver_override);
 }
 
 static ssize_t driver_override_store(struct device *_dev,
