f3a1dd23e3cb257eddfce65838f77f92333d6ea8
Geert Uytterhoeven <geert+renesas@glider.be>
2018-04-10 15:21:44 +0200

    ARM: amba: Fix race condition with driver_override
    commit 6a7228d90d42bcacfe38786756ba62762b91c20a upstream.
    
    The driver_override implementation is susceptible to a race condition
    when different threads are reading vs storing a different driver
    override.  Add locking to avoid this race condition.
    
    Cfr. commits 6265539776a0810b ("driver core: platform: fix race
    condition with driver_override") and 9561475db680f714 ("PCI: Fix race
    condition with driver_override").
    
    Fixes: 3cf385713460eb2b ("ARM: 8256/1: driver coamba: add device binding path 'driver_override'")
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Todd Kjos <tkjos@google.com>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/amba/bus.c
+++ drivers/amba/bus.c
@@ -70,7 +70,10 @@ static ssize_t driver_override_show(struct device *_dev,
 	struct amba_device *dev = to_amba_device(_dev);
 	ssize_t len;
 
-	return sprintf(buf, "%s\n", dev->driver_override);
+	device_lock(_dev);
+	len = sprintf(buf, "%s\n", dev->driver_override);
+	device_unlock(_dev);
+	return len;
 }
 
 static ssize_t driver_override_store(struct device *_dev,
