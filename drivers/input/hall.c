f4c278aeefdf9d5328475b8db6405490cbc8ab62
ananjaser1211 <ananjaser@gmail.com>
2020-05-14 02:10:37 +0300

    [TEMP]drivers/input: add HALL_EVENT_REVERSE - this will be defined in config in case of building for android 10 until a better workaround can be added
    Signed-off-by: corsicanu <corsicanu22@gmail.com>

--- drivers/input/hall.c
+++ drivers/input/hall.c
@@ -82,7 +82,12 @@ static void flip_cover_work(struct work_struct *work)
 	if(first == second) {
 		flip_cover = first;
 
+#ifdef CONFIG_HALL_EVENT_REVERSE
+		input_report_switch(ddata->input, ddata->event_val, !flip_cover);
+#else
 		input_report_switch(ddata->input, ddata->event_val, flip_cover);
+
+#endif
 		input_sync(ddata->input);
 	}
 }
@@ -101,7 +106,12 @@ static void flip_cover_work(struct work_struct *work)
 
 	flip_cover = first;
 
+#ifdef CONFIG_HALL_EVENT_REVERSE
+	input_report_switch(ddata->input, ddata->event_val, !flip_cover);
+#else
 	input_report_switch(ddata->input, ddata->event_val, flip_cover);
+
+#endif
 	input_sync(ddata->input);
 }
 #endif
