7a63f988149b344f1f2ceb367f49470f6a444fc4
corsicanu <corsicanu22@gmail.com>
2020-09-18 13:06:28 +0300

    arch/smp: fix upstream derp

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -899,7 +899,7 @@ void smp_send_stop(void)
 	while (num_other_online_cpus() && timeout--)
 		udelay(1);
 
-	if (num_other_online_cpus())
+	if (num_other_online_cpus()) {
 		pr_warning("SMP: failed to stop secondary CPUs\n");
 	} else {
 		pr_info("SMP: completed to stop secondary CPUS\n");
