d9a362e596d1c6e71ec4de220e91d68806938eea
Anan Jaser <ananjaser@gmail.com>
2020-09-21 01:54:10 +0400

    Revert "arm64: mm: Add trace_irqflags annotations to do_debug_exception()"
    This reverts commit 7603ce0747626a07fb3b767060a33529711da439.

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -722,14 +722,9 @@ asmlinkage int __exception do_debug_exception(unsigned long addr,
 {
 	const struct fault_info *inf = debug_fault_info + DBG_ESR_EVT(esr);
 	struct siginfo info;
-	int rv;
 
-	/*
-	 * Tell lockdep we disabled irqs in entry.S. Do nothing if they were
-	 * already disabled to preserve the last enabled/disabled addresses.
-	 */
-	if (interrupts_enabled(regs))
-		trace_hardirqs_off();
+	if (!inf->fn(addr, esr, regs))
+		return 1;
 
 	if (unhandled_signal(current, inf->sig)
 	    && show_unhandled_signals_ratelimited())
@@ -742,7 +737,7 @@ asmlinkage int __exception do_debug_exception(unsigned long addr,
 	info.si_addr  = (void __user *)addr;
 	arm64_notify_die("Oops - Debug exception", regs, &info, 0);
 
-	return rv;
+	return 0;
 }
 
 #ifdef CONFIG_ARM64_PAN
