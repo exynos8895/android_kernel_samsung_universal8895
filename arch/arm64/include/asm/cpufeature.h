1df4ad7875dd36a46c6af710d4ecd79d496b07ad
Anan Jaser <ananjaser@gmail.com>
2020-09-21 16:45:32 +0400

    Revert "arm64: capabilities: Handle sign of the feature bit"
    This reverts commit 46cffb4859f2945bbdcc850ba210e594c1994687.

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -92,10 +92,9 @@ struct arm64_cpu_capabilities {
 
 		struct {	/* Feature register checking */
 			u32 sys_reg;
-			u8 field_pos;
-			u8 min_field_value;
-			u8 hwcap_type;
-			bool sign;
+			int field_pos;
+			int min_field_value;
+			int hwcap_type;
 			unsigned long hwcap;
 		};
 	};
