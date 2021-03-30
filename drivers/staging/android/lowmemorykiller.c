a8ae12e7720a8298fdee7f4ae7feb24ef8756842
corsicanu <me@corsicanu.ro>
2021-03-30 11:40:19 +0300

    lowmemorykiller: update to 4.4 google-common

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -32,18 +32,20 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
-#include <linux/cma.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
 #include <linux/swap.h>
 #include <linux/rcupdate.h>
 #include <linux/profile.h>
 #include <linux/notifier.h>
+#include <linux/circ_buf.h>
+#include <linux/proc_fs.h>
+#include <linux/slab.h>
+#include <linux/poll.h>
 
 #define CREATE_TRACE_POINTS
 #include "trace/lowmemorykiller.h"
@@ -63,9 +65,6 @@ static int lowmem_minfree[6] = {
 	16 * 1024,	/* 64MB */
 };
 static int lowmem_minfree_size = 4;
-static uint32_t lowmem_lmkcount = 0;
-static int lmkd_count;
-static int lmkd_cricount;
 
 static unsigned long lowmem_deathpending_timeout;
 
@@ -75,82 +74,138 @@ static unsigned long lowmem_deathpending_timeout;
 			pr_info(x);			\
 	} while (0)
 
-#if defined(CONFIG_ZSWAP)
-extern u64 zswap_pool_pages;
-extern atomic_t zswap_stored_pages;
-#endif
 
-static int test_task_flag(struct task_struct *p, int flag)
+static DECLARE_WAIT_QUEUE_HEAD(event_wait);
+static DEFINE_SPINLOCK(lmk_event_lock);
+static struct circ_buf event_buffer;
+#define MAX_BUFFERED_EVENTS 8
+#define MAX_TASKNAME 128
+
+struct lmk_event {
+	char taskname[MAX_TASKNAME];
+	pid_t pid;
+	uid_t uid;
+	pid_t group_leader_pid;
+	unsigned long min_flt;
+	unsigned long maj_flt;
+	unsigned long rss_in_pages;
+	short oom_score_adj;
+	short min_score_adj;
+	unsigned long long start_time;
+	struct list_head list;
+};
+
+void handle_lmk_event(struct task_struct *selected, int selected_tasksize,
+		      short min_score_adj)
 {
-	struct task_struct *t = p;
+	int head;
+	int tail;
+	struct lmk_event *events;
+	struct lmk_event *event;
 
-	do {
-		if (test_tsk_thread_flag(t, flag))
-			return 1;
-	} while_each_thread(p, t);
+	spin_lock(&lmk_event_lock);
 
+	head = event_buffer.head;
+	tail = READ_ONCE(event_buffer.tail);
+
+	/* Do not continue to log if no space remains in the buffer. */
+	if (CIRC_SPACE(head, tail, MAX_BUFFERED_EVENTS) < 1) {
+		spin_unlock(&lmk_event_lock);
+		return;
+	}
+
+	events = (struct lmk_event *) event_buffer.buf;
+	event = &events[head];
+
+	strncpy(event->taskname, selected->comm, MAX_TASKNAME);
+
+	event->pid = selected->pid;
+	event->uid = from_kuid_munged(current_user_ns(), task_uid(selected));
+	if (selected->group_leader)
+		event->group_leader_pid = selected->group_leader->pid;
+	else
+		event->group_leader_pid = -1;
+	event->min_flt = selected->min_flt;
+	event->maj_flt = selected->maj_flt;
+	event->oom_score_adj = selected->signal->oom_score_adj;
+	event->start_time = nsec_to_clock_t(selected->real_start_time);
+	event->rss_in_pages = selected_tasksize;
+	event->min_score_adj = min_score_adj;
+
+	event_buffer.head = (head + 1) & (MAX_BUFFERED_EVENTS - 1);
+
+	spin_unlock(&lmk_event_lock);
+
+	wake_up_interruptible(&event_wait);
+}
+
+static int lmk_event_show(struct seq_file *s, void *unused)
+{
+	struct lmk_event *events = (struct lmk_event *) event_buffer.buf;
+	int head;
+	int tail;
+	struct lmk_event *event;
+
+	spin_lock(&lmk_event_lock);
+
+	head = event_buffer.head;
+	tail = event_buffer.tail;
+
+	if (head == tail) {
+		spin_unlock(&lmk_event_lock);
+		return -EAGAIN;
+	}
+
+	event = &events[tail];
+
+	seq_printf(s, "%lu %lu %lu %lu %lu %lu %hd %hd %llu\n%s\n",
+		(unsigned long) event->pid, (unsigned long) event->uid,
+		(unsigned long) event->group_leader_pid, event->min_flt,
+		event->maj_flt, event->rss_in_pages, event->oom_score_adj,
+		event->min_score_adj, event->start_time, event->taskname);
+
+	event_buffer.tail = (tail + 1) & (MAX_BUFFERED_EVENTS - 1);
+
+	spin_unlock(&lmk_event_lock);
 	return 0;
 }
 
-static void show_memory(void)
+static unsigned int lmk_event_poll(struct file *file, poll_table *wait)
 {
-	unsigned long nr_rbin_free, nr_rbin_pool, nr_rbin_alloc, nr_rbin_file;
+	int ret = 0;
 
-	nr_rbin_free = global_page_state(NR_FREE_RBIN_PAGES);
-	nr_rbin_pool = atomic_read(&rbin_pool_pages);
-	nr_rbin_alloc = atomic_read(&rbin_allocated_pages);
-	nr_rbin_file = totalrbin_pages - nr_rbin_free - nr_rbin_pool
-					- nr_rbin_alloc;
+	poll_wait(file, &event_wait, wait);
+	spin_lock(&lmk_event_lock);
+	if (event_buffer.head != event_buffer.tail)
+		ret = POLLIN;
+	spin_unlock(&lmk_event_lock);
+	return ret;
+}
 
-#define K(x) ((x) << (PAGE_SHIFT - 10))
-	printk("Mem-Info:"
-		" totalram_pages:%lukB"
-		" free:%lukB"
-		" active_anon:%lukB"
-		" inactive_anon:%lukB"
-		" active_file:%lukB"
-		" inactive_file:%lukB"
-		" unevictable:%lukB"
-		" isolated(anon):%lukB"
-		" isolated(file):%lukB"
-		" dirty:%lukB"
-		" writeback:%lukB"
-		" mapped:%lukB"
-		" shmem:%lukB"
-		" slab_reclaimable:%lukB"
-		" slab_unreclaimable:%lukB"
-		" kernel_stack:%lukB"
-		" pagetables:%lukB"
-		" free_cma:%lukB"
-		" rbin_free:%lukB"
-		" rbin_pool:%lukB"
-		" rbin_alloc:%lukB"
-		" rbin_file:%lukB"
-		"\n",
-		K(totalram_pages),
-		K(global_page_state(NR_FREE_PAGES)),
-		K(global_page_state(NR_ACTIVE_ANON)),
-		K(global_page_state(NR_INACTIVE_ANON)),
-		K(global_page_state(NR_ACTIVE_FILE)),
-		K(global_page_state(NR_INACTIVE_FILE)),
-		K(global_page_state(NR_UNEVICTABLE)),
-		K(global_page_state(NR_ISOLATED_ANON)),
-		K(global_page_state(NR_ISOLATED_FILE)),
-		K(global_page_state(NR_FILE_DIRTY)),
-		K(global_page_state(NR_WRITEBACK)),
-		K(global_page_state(NR_FILE_MAPPED)),
-		K(global_page_state(NR_SHMEM)),
-		K(global_page_state(NR_SLAB_RECLAIMABLE)),
-		K(global_page_state(NR_SLAB_UNRECLAIMABLE)),
-		K(global_page_state(NR_KERNEL_STACK)),
-		K(global_page_state(NR_PAGETABLE)),
-		K(global_page_state(NR_FREE_CMA_PAGES)),
-		K(nr_rbin_free),
-		K(nr_rbin_pool),
-		K(nr_rbin_alloc),
-		K(nr_rbin_file)
-		);
-#undef K
+static int lmk_event_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, lmk_event_show, inode->i_private);
+}
+
+static const struct file_operations event_file_ops = {
+	.open = lmk_event_open,
+	.poll = lmk_event_poll,
+	.read = seq_read
+};
+
+static void lmk_event_init(void)
+{
+	struct proc_dir_entry *entry;
+
+	event_buffer.head = 0;
+	event_buffer.tail = 0;
+	event_buffer.buf = kmalloc(
+		sizeof(struct lmk_event) * MAX_BUFFERED_EVENTS, GFP_KERNEL);
+	if (!event_buffer.buf)
+		return;
+	entry = proc_create("lowmemorykiller", 0, NULL, &event_file_ops);
+	if (!entry)
+		pr_err("error creating kernel lmk event file\n");
 }
 
 static unsigned long lowmem_count(struct shrinker *s,
@@ -179,17 +234,6 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 						global_page_state(NR_SHMEM) -
 						global_page_state(NR_UNEVICTABLE) -
 						total_swapcache_pages();
-	unsigned long nr_rbin_free, nr_rbin_pool, nr_rbin_alloc, nr_rbin_file;
-
-	if ((sc->gfp_mask & __GFP_RBIN) != __GFP_RBIN) {
-		nr_rbin_free = global_page_state(NR_FREE_RBIN_PAGES);
-		nr_rbin_pool = atomic_read(&rbin_pool_pages);
-		nr_rbin_alloc = atomic_read(&rbin_allocated_pages);
-		nr_rbin_file = totalrbin_pages - nr_rbin_free - nr_rbin_pool
-						- nr_rbin_alloc;
-		other_free -= nr_rbin_free;
-		other_file -= nr_rbin_file;
-	}
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
@@ -223,27 +267,15 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		if (tsk->flags & PF_KTHREAD)
 			continue;
 
-		if (same_thread_group(tsk, current))
-			continue;
-
-#if defined(CONFIG_ARM) || defined(CONFIG_ARM64)
-		if (test_task_flag(tsk, TIF_MEMALLOC))
-			continue;
-#endif
 		p = find_lock_task_mm(tsk);
 		if (!p)
 			continue;
 
-		if (test_tsk_thread_flag(p, TIF_MEMDIE)) {
+		if (test_tsk_thread_flag(p, TIF_MEMDIE) &&
+		    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
 			task_unlock(p);
-
-			if (time_before_eq(jiffies,
-					   lowmem_deathpending_timeout)) {
-				rcu_read_unlock();
-				return 0;
-			}
-
-			continue;
+			rcu_read_unlock();
+			return 0;
 		}
 		oom_score_adj = p->signal->oom_score_adj;
 		if (oom_score_adj < min_score_adj) {
@@ -251,14 +283,6 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			continue;
 		}
 		tasksize = get_mm_rss(p->mm);
-#if defined(CONFIG_ZSWAP)
-		if (atomic_read(&zswap_stored_pages)) {
-			lowmem_print(3, "shown tasksize : %d\n", tasksize);
-			tasksize += (int)zswap_pool_pages * get_mm_counter(p->mm, MM_SWAPENTS)
-				/ atomic_read(&zswap_stored_pages);
-			lowmem_print(3, "real tasksize : %d\n", tasksize);
-		}
-#endif
 		task_unlock(p);
 		if (tasksize <= 0)
 			continue;
@@ -302,15 +326,19 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			     cache_size, cache_limit,
 			     min_score_adj,
 			     free);
-		show_memory();
 		lowmem_deathpending_timeout = jiffies + HZ;
 		rem += selected_tasksize;
-		lowmem_lmkcount++;
+		get_task_struct(selected);
 	}
 
 	lowmem_print(4, "lowmem_scan %lu, %x, return %lu\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
+
+	if (selected) {
+		handle_lmk_event(selected, selected_tasksize, min_score_adj);
+		put_task_struct(selected);
+	}
 	return rem;
 }
 
@@ -323,13 +351,10 @@ static struct shrinker lowmem_shrinker = {
 static int __init lowmem_init(void)
 {
 	register_shrinker(&lowmem_shrinker);
+	lmk_event_init();
 	return 0;
 }
-
-static void __exit lowmem_exit(void)
-{
-	unregister_shrinker(&lowmem_shrinker);
-}
+device_initcall(lowmem_init);
 
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_AUTODETECT_OOM_ADJ_VALUES
 static short lowmem_oom_adj_to_oom_score_adj(short oom_adj)
@@ -425,11 +450,4 @@ module_param_array_named(adj, lowmem_adj, short, &lowmem_adj_size,
 module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
 			 S_IRUGO | S_IWUSR);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
-module_param_named(lmkcount, lowmem_lmkcount, uint, S_IRUGO);
-module_param_named(lmkd_count, lmkd_count, int, 0644);
-module_param_named(lmkd_cricount, lmkd_cricount, int, 0644);
 
-module_init(lowmem_init);
-module_exit(lowmem_exit);
-
-MODULE_LICENSE("GPL");
