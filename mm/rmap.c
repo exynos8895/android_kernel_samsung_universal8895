5657eb5bbbaa8ef362c7a12bf90f7708dc13a7b2
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-10-12 17:51:03 +0000

    mm: rmap: Fix invalid casting from a void to an enumerator.  fixes:
    ../mm/rmap.c:1344:25: warning: cast to smaller integer type 'enum ttu_flags' from 'void *' [-Wvoid-pointer-to-enum-cast]
            enum ttu_flags flags = (enum ttu_flags)arg;
                                   ^~~~~~~~~~~~~~~~~~~

--- mm/rmap.c
+++ mm/rmap.c
@@ -1327,7 +1327,7 @@ static int try_to_unmap_one(struct page *page, struct vm_area_struct *vma,
 	pte_t pteval;
 	spinlock_t *ptl;
 	int ret = SWAP_AGAIN;
-	enum ttu_flags flags = (enum ttu_flags)arg;
+	enum ttu_flags flags = (enum ttu_flags)(long)arg;
 
 	/* munlock has nothing to gain from examining un-locked vmas */
 	if ((flags & TTU_MUNLOCK) && !(vma->vm_flags & VM_LOCKED))
